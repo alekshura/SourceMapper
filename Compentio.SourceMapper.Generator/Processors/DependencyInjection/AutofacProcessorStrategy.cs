48d6a2242f8b731fade5f20e7e6d9a402fd59171
piotr <piotr.szulc.zgora@gmail.com>
2021-11-03 08:15:36 +0100

    Code generation for Autofac processor.

--- Compentio.SourceMapper.Generator/Processors/DependencyInjection/AutofacProcessorStrategy.cs
+++ Compentio.SourceMapper.Generator/Processors/DependencyInjection/AutofacProcessorStrategy.cs
@@ -7,7 +7,40 @@ namespace Compentio.SourceMapper.Processors.DependencyInjection
     {
         public IResult GenerateCode(ISourcesMetadata sourcesMetadata)
         {
-            throw new NotImplementedException();
+            var result = @$"// <mapper-source-generated />
+                            // <generated-at '{System.DateTime.UtcNow}' />
+
+            using System;
+            using Microsoft.Extensions.DependencyInjection;
+            using Autofac;
+            using Autofac.Extensions.DependencyInjection;
+
+            { $"namespace Compentio.SourceMapper.DependencyInjection"}
+            {{
+               public static class {sourcesMetadata.DependencyInjection.DependencyInjectionClassName}
+               {{                  
+                   public static ContainerBuilder AddMappers(this ContainerBuilder builder)
+                   {{
+                        { GenerateBuilder(sourcesMetadata) }
+                        return builder;
+                   }}                                  
+               }}
+            }}
+            ";
+
+            return Result.Ok(result);
+        }
+
+        private string GenerateBuilder(ISourcesMetadata sourcesMetadata)
+        {
+            var builder = string.Empty;
+
+            foreach (var mapper in sourcesMetadata.Mappers)
+            {
+                builder += $"builder.RegisterType<{mapper.Namespace}.{mapper.TargetClassName}>().As<{mapper.Namespace}.{mapper.Name}>().SingleInstance();";
+            }
+
+            return builder;
         }
     }
 }
