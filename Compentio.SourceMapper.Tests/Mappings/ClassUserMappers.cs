f10d31aca607abefedffa4c61565de03200e169b
Piotr Szulc <piotr.szulc.zgora@gmail.com>
2021-12-09 11:04:32 +0100

    Update - no entities.

--- Compentio.SourceMapper.Tests/Mappings/ClassUserMappers.cs
+++ Compentio.SourceMapper.Tests/Mappings/ClassUserMappers.cs
@@ -1,84 +1,84 @@
-﻿using Compentio.SourceMapper.Attributes;
-using Compentio.SourceMapper.Tests.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using Compentio.SourceMapper.Attributes;
+//using Compentio.SourceMapper.Tests.Entities;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
-namespace Compentio.SourceMapper.Tests.Mappings
-{
-    /// <summary>
-    /// Abstract class mapper with examples of expressions for additional mappings
-    /// </summary>
-    [Mapper(ClassName = "ClassUserDaoMapper")]
-    public abstract partial class UserMapper
-    {
-        [Mapping(Source = nameof(UserDao.UserId), Target = nameof(UserInfo.Id), Expression = nameof(ConvertUserId))]
-        [Mapping(Source = nameof(UserDao.UserGender), Target = nameof(UserInfo.Sex), Expression = nameof(ConvertUserGender))]
-        [Mapping(Source = nameof(UserDao), Target = nameof(UserInfo.Name), Expression = nameof(ConvertUserName))]
-        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
-        public abstract UserInfo MapToDomainModel(UserDao source);
+//namespace Compentio.SourceMapper.Tests.Mappings
+//{
+//    /// <summary>
+//    /// Abstract class mapper with examples of expressions for additional mappings
+//    /// </summary>
+//    [Mapper(ClassName = "ClassUserDaoMapper")]
+//    public abstract partial class UserMapper
+//    {
+//        [Mapping(Source = nameof(UserDao.UserId), Target = nameof(UserInfo.Id), Expression = nameof(ConvertUserId))]
+//        [Mapping(Source = nameof(UserDao.UserGender), Target = nameof(UserInfo.Sex), Expression = nameof(ConvertUserGender))]
+//        [Mapping(Source = nameof(UserDao), Target = nameof(UserInfo.Name), Expression = nameof(ConvertUserName))]
+//        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
+//        public abstract UserInfo MapToDomainModel(UserDao source);
 
-        protected static int ConvertUserId(long id)
-        {
-            return Convert.ToInt32(id);
-        }
+//        protected static int ConvertUserId(long id)
+//        {
+//            return Convert.ToInt32(id);
+//        }
 
-        protected static long ConvertId(int id)
-        {
-            return Convert.ToInt64(id);
-        }
+//        protected static long ConvertId(int id)
+//        {
+//            return Convert.ToInt64(id);
+//        }
 
-        protected static string ConvertUserName(UserDao userDao) => $"{userDao.FirstName} {userDao.LastName}";
+//        protected static string ConvertUserName(UserDao userDao) => $"{userDao.FirstName} {userDao.LastName}";
 
-        protected readonly Func<UserGender, Sex> ConvertUserGender = gender => gender == UserGender.Female ? Sex.W : Sex.M;
-        protected readonly Func<Sex, UserGender> ConvertSex = sex => sex == Sex.M ? UserGender.Male : UserGender.Female;
-    }
+//        protected readonly Func<UserGender, Sex> ConvertUserGender = gender => gender == UserGender.Female ? Sex.W : Sex.M;
+//        protected readonly Func<Sex, UserGender> ConvertSex = sex => sex == Sex.M ? UserGender.Male : UserGender.Female;
+//    }
 
-    [Mapper(ClassName = "ClassUserDaoArrayMapper")]
-    public abstract partial class UserDataArrayMapper
-    {
-        [Mapping(Source = nameof(UserWithArrayDao.UserAddresses), Target = nameof(UserInfoWithArray.Addresses), Expression = nameof(ConvertAddresses))]
-        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
-        public abstract UserInfoWithArray MapToDomainModel(UserWithArrayDao source);
+//    [Mapper(ClassName = "ClassUserDaoArrayMapper")]
+//    public abstract partial class UserDataArrayMapper
+//    {
+//        [Mapping(Source = nameof(UserWithArrayDao.UserAddresses), Target = nameof(UserInfoWithArray.Addresses), Expression = nameof(ConvertAddresses))]
+//        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
+//        public abstract UserInfoWithArray MapToDomainModel(UserWithArrayDao source);
 
-        protected Address[] ConvertAddresses(AddressDao[] addresses)
-        {
-            return addresses.Select(a => MapAddress(a)).ToArray();
-        }
+//        protected Address[] ConvertAddresses(AddressDao[] addresses)
+//        {
+//            return addresses.Select(a => MapAddress(a)).ToArray();
+//        }
 
-        protected AddressDao[] ConvertAddressesDao(Address[] addresses)
-        {
-            return addresses.Select(a => MapFromAddress(a)).ToArray();
-        }
+//        protected AddressDao[] ConvertAddressesDao(Address[] addresses)
+//        {
+//            return addresses.Select(a => MapFromAddress(a)).ToArray();
+//        }
 
-        [InverseMapping(InverseMethodName = "MapFromAddress")]
-        public abstract Address MapAddress(AddressDao source);
+//        [InverseMapping(InverseMethodName = "MapFromAddress")]
+//        public abstract Address MapAddress(AddressDao source);
 
-        [InverseMapping(InverseMethodName = "MapFromRegion")]
-        public abstract Region MapRegion(RegionDao source);
-    }
+//        [InverseMapping(InverseMethodName = "MapFromRegion")]
+//        public abstract Region MapRegion(RegionDao source);
+//    }
 
-    [Mapper(ClassName = "ClassUserDaoListMapper")]
-    public abstract partial class UserDataListMapper
-    {
-        [Mapping(Source = nameof(UserWithListDao.UserAddresses), Target = nameof(UserInfoWithList.Addresses), Expression = nameof(ConvertAddresses))]
-        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
-        public abstract UserInfoWithList MapToDomainModel(UserWithListDao source);
+//    [Mapper(ClassName = "ClassUserDaoListMapper")]
+//    public abstract partial class UserDataListMapper
+//    {
+//        [Mapping(Source = nameof(UserWithListDao.UserAddresses), Target = nameof(UserInfoWithList.Addresses), Expression = nameof(ConvertAddresses))]
+//        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
+//        public abstract UserInfoWithList MapToDomainModel(UserWithListDao source);
 
-        protected IList<Address> ConvertAddresses(IList<AddressDao> addresses)
-        {
-            return addresses.Select(a => MapAddress(a)).ToList();
-        }
+//        protected IList<Address> ConvertAddresses(IList<AddressDao> addresses)
+//        {
+//            return addresses.Select(a => MapAddress(a)).ToList();
+//        }
 
-        protected IList<AddressDao> ConvertAddressesDao(IList<Address> addresses)
-        {
-            return addresses.Select(a => MapFromAddress(a)).ToList();
-        }
+//        protected IList<AddressDao> ConvertAddressesDao(IList<Address> addresses)
+//        {
+//            return addresses.Select(a => MapFromAddress(a)).ToList();
+//        }
 
-        [InverseMapping(InverseMethodName = "MapFromAddress")]
-        public abstract Address MapAddress(AddressDao source);
+//        [InverseMapping(InverseMethodName = "MapFromAddress")]
+//        public abstract Address MapAddress(AddressDao source);
 
-        [InverseMapping(InverseMethodName = "MapFromRegion")]
-        public abstract Region MapRegion(RegionDao source);
-    }
-}
+//        [InverseMapping(InverseMethodName = "MapFromRegion")]
+//        public abstract Region MapRegion(RegionDao source);
+//    }
+//}
--- Compentio.SourceMapper.Tests/Mappings/InterfaceUserMappers.cs
+++ Compentio.SourceMapper.Tests/Mappings/InterfaceUserMappers.cs
@@ -1,48 +1,48 @@
-﻿using Compentio.SourceMapper.Attributes;
-using Compentio.SourceMapper.Tests.Entities;
+﻿//using Compentio.SourceMapper.Attributes;
+//using Compentio.SourceMapper.Tests.Entities;
 
-namespace Compentio.SourceMapper.Tests.Mappings
-{
-    [Mapper(ClassName = "InterfaceUserDaoMapper")]
-    public partial interface IUserDaoMapper
-    {
-        [Mapping(Source = nameof(UserDao.FirstName), Target = nameof(UserInfo.Name))]
-        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
-        UserInfo MapToDomainModel(UserDao source);
+//namespace Compentio.SourceMapper.Tests.Mappings
+//{
+//    [Mapper(ClassName = "InterfaceUserDaoMapper")]
+//    public partial interface IUserDaoMapper
+//    {
+//        [Mapping(Source = nameof(UserDao.FirstName), Target = nameof(UserInfo.Name))]
+//        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
+//        UserInfo MapToDomainModel(UserDao source);
 
-        [InverseMapping(InverseMethodName = "MapFromAddress")]
-        Address MapAddress(UserDao source);
+//        [InverseMapping(InverseMethodName = "MapFromAddress")]
+//        Address MapAddress(UserDao source);
 
-        [InverseMapping(InverseMethodName = "MapFromRegion")]
-        Region MapRegion(UserDao source);
-    }
+//        [InverseMapping(InverseMethodName = "MapFromRegion")]
+//        Region MapRegion(UserDao source);
+//    }
 
-    [Mapper(ClassName = "InterfaceUserDataDaoMapper")]
-    public partial interface IUserDataDaoMapper
-    {
-        [Mapping(Source = nameof(UserDataDao.FirstName), Target = nameof(UserInfo.Name))]
-        [Mapping(Source = nameof(UserDataDao.UserAddress), Target = nameof(UserInfo.Address))]
-        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
-        UserInfo MapToDomainModel(UserDataDao source);
+//    [Mapper(ClassName = "InterfaceUserDataDaoMapper")]
+//    public partial interface IUserDataDaoMapper
+//    {
+//        [Mapping(Source = nameof(UserDataDao.FirstName), Target = nameof(UserInfo.Name))]
+//        [Mapping(Source = nameof(UserDataDao.UserAddress), Target = nameof(UserInfo.Address))]
+//        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
+//        UserInfo MapToDomainModel(UserDataDao source);
 
-        [InverseMapping(InverseMethodName = "MapFromAddress")]
-        Address MapAddress(AddressDao addressDao);
+//        [InverseMapping(InverseMethodName = "MapFromAddress")]
+//        Address MapAddress(AddressDao addressDao);
 
-        [InverseMapping(InverseMethodName = "MapFromRegion")]
-        Region MapRegion(RegionDao regionDao);
-    }
+//        [InverseMapping(InverseMethodName = "MapFromRegion")]
+//        Region MapRegion(RegionDao regionDao);
+//    }
 
-    [Mapper(ClassName = "InterfaceUserDaoArrayMapper")]
-    public partial interface IUserDataArrayMapper
-    {
-        [Mapping(Source = nameof(UserWithArrayDao.FirstName), Target = nameof(UserInfoWithArray.Name))]
-        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
-        UserInfoWithArray MapToDomainModel(UserWithArrayDao userWithArrayDao);
+//    [Mapper(ClassName = "InterfaceUserDaoArrayMapper")]
+//    public partial interface IUserDataArrayMapper
+//    {
+//        [Mapping(Source = nameof(UserWithArrayDao.FirstName), Target = nameof(UserInfoWithArray.Name))]
+//        [InverseMapping(InverseMethodName = "MapToDatabaseModel")]
+//        UserInfoWithArray MapToDomainModel(UserWithArrayDao userWithArrayDao);
 
-        [InverseMapping(InverseMethodName = "MapFromAddress")]
-        Address MapAddress(AddressDao addressDao);
+//        [InverseMapping(InverseMethodName = "MapFromAddress")]
+//        Address MapAddress(AddressDao addressDao);
 
-        [InverseMapping(InverseMethodName = "MapFromRegion")]
-        Region MapRegion(RegionDao regionDao);
-    }
-}
+//        [InverseMapping(InverseMethodName = "MapFromRegion")]
+//        Region MapRegion(RegionDao regionDao);
+//    }
+//}
